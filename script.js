6cfb3977af610379840fa21569b55129e0fc1652
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-07-11 11:48:31 +0530

    feat: draw on the canvas

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+function mouseOverDiv(e) {
+  e.target.style.backgroundColor = "purple";
+}
 const container = document.querySelector('.container');
 
 const numberOfSquares = prompt("How many divs you want?");
@@ -6,6 +10,7 @@ for (let i = 0; i < numberOfSquares**2; i++) {
   const tempDiv = document.createElement('div');
   tempDiv.classList.add('square');
   tempDiv.setAttribute('style', `width :calc(600px/${numberOfSquares});aspect-ratio: 1/1`);
+  tempDiv.addEventListener('mouseover', mouseOverDiv);
   container.appendChild(tempDiv);
   
 }
